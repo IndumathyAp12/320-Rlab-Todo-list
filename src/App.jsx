b6999ad982317a180d24d74ad1b7bad511ba8c07
Indu Prabha <apindhuece12@gmail.com>
2024-06-16 23:01:10 -0400

    all the codes are organised in seperate folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
-import React, { useReducer, useState } from 'react';
-import { initialState, todoReducer } from './reducer';
-import  './App.css'; 
-import TodoItem from './Todo-item';
+import React, { useReducer } from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import { todoReducer, initialState, ACTIONS } from './todoReducer';
 
-function App() {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
-  const [newTodo, setNewTodo] = useState('');
-
-  const handleAddTodo = () => {
-    if (newTodo.trim()) {
-      dispatch({ type: 'ADD_TODO', title: newTodo });
-      setNewTodo('');
-    }
-  };
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <div>
-        <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Add a new todo"
-        />
-        <button onClick={handleAddTodo}>Add</button>
-      </div>
-      <ul>
-        {state.map(todo => (
-          <TodoItem key={todo.id} todo={todo} dispatch={dispatch} />
-        ))}
-      </ul>
+      <TodoForm dispatch={dispatch} />
+      <TodoList
+        todos={todos}
+        dispatch={dispatch}
+      />
     </div>
   );
-}
+};
 
 export default App;
